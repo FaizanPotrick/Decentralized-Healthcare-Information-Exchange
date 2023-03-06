10587c1f64ef3660cb5d72d89d67c47a4f53360d
FaizanPotrick <faizan.potrick02@gmail.com>
2023-03-06 17:44:46 +0530

    Add alert and loading

---
+++ client/src/components/Alert.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useContext } from "react";
+import { StateContext } from "../context/StateContext";
+
+const Alert = () => {
+  const { alert, setAlert } = useContext(StateContext);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setAlert({ isAlert: false, type: "", message: "" });
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, [alert]);
+
+  return (
+    alert.isAlert && (
+      <div
+        className={`z-50 fixed bottom-4 left-4 flex p-4 text-sm ${
+          alert.type === "error" ? "text-red-800" : "text-green-800"
+        } border ${
+          alert.type === "error" ? "border-red-300" : "border-green-300"
+        } rounded-lg bg-red-50`}
+      >
+        <div>{alert.message}</div>
+      </div>
+    )
+  );
+};
+
+export default Alert;
