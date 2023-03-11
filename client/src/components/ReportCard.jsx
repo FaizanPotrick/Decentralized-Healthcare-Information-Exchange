3f402573e4bc3eca6690f13478ea2feb1e6b4af0
FaizanPotrick <faizan.potrick02@gmail.com>
2023-03-11 10:09:09 +0530

    Update Report UI

--- client/src/components/ReportCard.jsx
+++ client/src/components/ReportCard.jsx
@@ -11,16 +11,16 @@ const ReportCard = ({
   severity,
 }) => {
   return (
-    <div className="flex flex-col justify-around w-full max-w-md border-2 border-[#0e8f66]/20 shadow-lg rounded-lg bg-[#0e8f66]/[0.15] p-3 md:p-4">
+    <div className="flex flex-col justify-around h-full w-full max-w-md border border-gray-200/80 shadow-lg rounded-lg p-3 md:p-4 bg-white">
       <div className="flex justify-between items-start">
         <div className="flex flex-col">
-          <div className="relative text-[#00553a] text-xl md:text-2xl font-semibold">
+          <div className="relative text-slate-900 text-xl md:text-2xl font-semibold">
             {report_name}
             <span className="uppercase absolute bottom-3 text-xs font-semibold px-1.5 rounded shadow bg-blue-200 text-blue-800 ml-1">
               {report_type}
             </span>
           </div>
-          <div className="text-sm md:text-base text-[#00553a]/70 font-medium">
+          <div className="text-sm md:text-base text-slate-500/70">
             {patient_name} - {age}
           </div>
         </div>
@@ -37,7 +37,7 @@ const ReportCard = ({
         )}
       </div>
       <div className="flex items-center justify-between">
-        <div className="text-xl md:text-2xl font-bold text-[#00553a]">
+        <div className="text-xl md:text-2xl font-bold text-gray-900">
           ${report_cost}
         </div>
         <div className="bg-red-200 rounded-lg shadow text-xs md:text-sm font-medium px-3.5 py-0.5 text-red-700">
@@ -47,9 +47,7 @@ const ReportCard = ({
           Add to Cart
         </button>
       </div>
-      <div className="text-xs text-[#00553a]/70 font-medium">
-        {Date().substring(0, 15)}
-      </div>
+      <div className="text-xs text-slate-500/70">{Date().substring(0, 15)}</div>
     </div>
   );
 };
