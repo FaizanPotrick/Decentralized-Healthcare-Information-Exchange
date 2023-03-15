5bfe468665f39d10a661947ac7527eb1f15aa70e
darshikapongallu <darshikapongallu456@gmail.com>
2023-03-15 21:47:35 +0530

    Dropdown added

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -3,7 +3,7 @@ import { StateContext } from "../context/StateContext";
 import ImageLight from "../assets/login-image.png";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.jpg";
-
+import "../index.css";
 const Landing = () => {
   const { isLogin } = useContext(StateContext);
   return (
@@ -19,33 +19,62 @@ const Landing = () => {
               Dashboard
             </Link>
           )}
-          <Link
+          {/* <Link
             to="/login"
             className="inline-flex items-center space-x-3  text-white bg-purple-500 border-0 py-2 px-4 focus:outline-none hover:bg-purple-600 rounded-md text-base mt-4 md:mt-0"
           >
             Login
-          </Link>
+          </Link> */}
+        </div>
 
-          <Link
-            to="/registration/doctor"
-            className="inline-flex items-center text-white bg-purple-500 space-x-3 border-0 py-2 px-4 focus:outline-none hover:bg-purple-600 rounded-md  text-base mt-4 md:mt-0"
-          >
-            Doctor
-          </Link>
+        <div class="p-10 space-x-3">
+          <div class="dropdown inline-block relative">
+            <button class="bg-purple-500 text-white font-semibold py-2 px-4 rounded inline-flex items-center">
+              <span class="mr-1">Sign Up</span>
+              <svg
+                class="fill-current h-4 w-4"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />{" "}
+              </svg>
+            </button>
 
-          <Link
-            to="/registration/patient"
-            className="inline-flex items-center text-white bg-purple-500 space-x-3 border-0 py-2 px-4 focus:outline-none hover:bg-purple-600 rounded-md  text-base mt-4 md:mt-0"
-          >
-            Patient
-          </Link>
+            <ul class="dropdown-menu absolute hidden text-gray-700 pt-1">
+              <li class="">
+                <a
+                  class="rounded-t bg-purple-200 hover:bg-purple-300 text-black py-2 px-4 block whitespace-no-wrap"
+                  href="/registration/doctor"
+                >
+                  Doctor
+                </a>
+              </li>
+              <li class="">
+                <a
+                  class="bg-purple-200 hover:bg-purple-300 py-2 px-4  text-black block whitespace-no-wrap"
+                  href="/registration/patient"
+                >
+                  Patient
+                </a>
+              </li>
+              <li class="">
+                <a
+                  class="rounded-b bg-purple-200 hover:bg-purple-300 text-black py-2 px-4 block whitespace-no-wrap"
+                  href="/registration/buyer"
+                >
+                  Buyer
+                </a>
+              </li>
+            </ul>
+          </div>
 
-          <Link
-            to="/registration/buyer"
-            className="inline-flex items-center text-white bg-purple-500 space-x-3 border-0 py-2 px-4 focus:outline-none hover:bg-purple-600 rounded-md  text-base mt-4 md:mt-0"
-          >
-            Buyer
-          </Link>
+          <div class="dropdown inline-block relative ">
+            <button class="bg-purple-500 text-white font-semibold py-2 px-4 rounded inline-flex items-center">
+              <a href="/login">
+                <span class="mr-1">Login</span>
+              </a>
+            </button>
+          </div>
         </div>
       </header>
       <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center max-h-screen overflow-hidden">
@@ -60,7 +89,7 @@ const Landing = () => {
         </div>
         <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center overflow-hidden">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-            Revoltionizing healthcare with secure and transparent data exchange
+            Revolutionizing healthcare with secure and transparent data exchange
           </h1>
           <div className="flex w-full md:justify-start justify-center items-end">
             <Link
