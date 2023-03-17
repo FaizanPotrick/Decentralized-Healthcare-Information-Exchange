e4974fa0ac209acd728374a6232bf62b20bd7e0b
darshikapongallu <darshikapongallu456@gmail.com>
2023-03-17 21:34:15 +0530

    Dashboard Cards Updates

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -10,6 +10,10 @@ import Table from "../components/Table";
 import Cart from "../components/Cart";
 import axios from "axios";
 import revenue from "../icons/revenue.png";
+import report from "../assets/report.png";
+import sold from "../assets/sold.png";
+import sale from "../assets/sale.png";
+import revenues from "../assets/revenue.png";
 
 const Dashboard = () => {
   const { isLogin, setAlert, setLoading } = useContext(StateContext);
@@ -62,13 +66,58 @@ const Dashboard = () => {
           {/* <Link className="text-purple-600 hover:underline ml-1" to="/login">
           Redeem
         </Link> */}
-          <div className="flex items-center gap-6 mb-6 mt-10">
+          {/* <div className="flex items-center gap-6 mb-6 mt-10">
             <InfoCard title="Available Reports" value="60" icon={PeopleIcon} />
             <InfoCard title="For Sale" value="5" icon={MoneyIcon} />
             <InfoCard title="Sold" value="20" icon={CartIcon} />
             <InfoCard title="Revenue" value="₹64" icon={ChatIcon} />
-            {/* <img className="h-20 mt-2" src={revenue} /> */}
+            
+          </div> */}
+
+          <div className="flex items-center gap-12 mb-6 mt-10 ml-20">
+           
+            <div class=" w-64 p-4 bg-white border ml-5 border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex items-center">
+              <img class="h-20 w-20  mr-4" src={report} alt="report" />
+              <div>
+                <h5 class="mb-2 text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                  Available Reports
+                </h5>
+                <p>60</p>
+              </div>
+            </div>
+
+            <div class=" w-64 p-4 bg-white border ml-5 border-gray-200  rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex items-center">
+              <img class="h-20 w-20  mr-4" src={sale} alt="report" />
+              <div>
+                <h5 class="mb-2 text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                  For Sale
+                </h5>
+                <p>5</p>
+              </div>
+            </div>
+
+            <div class=" w-64 p-4 bg-white border ml-5 border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex items-center">
+              <img class="h-20 w-20  mr-4" src={sold} alt="report" />
+              <div>
+                <h5 class="mb-2 text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                  Sold
+                </h5>
+                <p>20</p>
+              </div>
+            </div>
+
+            <div class=" w-64 p-4 bg-white border  ml-5 border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex items-center">
+              <img class="h-20 w-20  mr-4" src={revenues} alt="report" />
+              <div>
+                <h5 class="mb-2 text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                  Revenue
+                </h5>
+                <p>₹64</p>
+              </div>
+            </div>
           </div>
+
+          <br />
 
           <Table
             head={[
