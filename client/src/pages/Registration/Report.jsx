45ef7468b3ecbc6739db727e21af6e4875754409
FaizanPotrick <faizan.potrick02@gmail.com>
2023-03-24 00:57:32 +0530

    Update Register Report

--- client/src/pages/Registration/Report.jsx
+++ client/src/pages/Registration/Report.jsx
@@ -4,22 +4,36 @@ import { useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 import Header from "../../components/Header";
 import axios from "axios";
+import { useForm } from "@mantine/form";
+import {
+  TextInput,
+  Paper,
+  Button,
+  Title,
+  Stack,
+  Container,
+  Select,
+  FileInput,
+} from "@mantine/core";
+import { DateInput } from "@mantine/dates";
 
 const Report = () => {
   const navigate = useNavigate();
   const { isLogin, setAlert, setLoading } = useContext(StateContext);
   const [cookies] = useCookies(["user_id"]);
   const [type_of_user] = useState(cookies.user_type);
-  const [register, setRegister] = useState({
-    patient_id: "",
-    name: "",
-    description: "",
-    patient_age: "",
-    type: "",
-    disease: "",
-    criticality: "",
-    date: "",
-    price: "",
+  const form = useForm({
+    initialValues: {
+      patient_id: "",
+      name: "",
+      description: "",
+      patient_age: "",
+      type: "",
+      disease: "",
+      criticality: "",
+      date: "",
+      price: "",
+    },
   });
   const [reportFile, setReportFile] = useState(null);
   const [patients, setPatients] = useState([]);
@@ -55,209 +69,198 @@ const Report = () => {
     }
   }, [type_of_user]);
 
-  const onChange = (e) => {
-    const { name, value } = e.target;
-    setRegister({ ...register, [name]: value });
-  };
-
-  const onFileChange = (e) => {
-    const { files } = e.target;
-    setReportFile(files[0]);
-  };
-
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    setLoading(true);
-    const formData = new FormData();
-    formData.append("patient_id", register.patient_id);
-    formData.append("name", register.name);
-    formData.append("description", register.description);
-    formData.append("patient_age", register.patient_age);
-    formData.append("type", register.type);
-    formData.append("disease", register.disease);
-    formData.append("criticality", register.criticality);
-    formData.append("date", register.date);
-    formData.append("price", register.price);
-    formData.append("report", reportFile);
-    try {
-     const {data} = await axios.post(`/api/registration/report/${type_of_user}`, formData);
-      setRegister({
-        patient_id: "",
-        name: "",
-        description: "",
-        patient_age: "",
-        type: "",
-        disease: "",
-        criticality: "",
-        date: "",
-        price: "",
-      });
-      setReportFile(null);
-      navigate("/dashboard");
-      setAlert({
-        isAlert: true,
-        type: "successfully uploaded",
-        message: data,
-      });
-    } catch (error) {
-      setAlert({
-        isAlert: true,
-        type: "error",
-        message: error.response.data,
-      });
-    }
-    setLoading(false);
-  };
   return (
-    <div className="h-full">
+    <>
       <Header />
-      <div className="flex items-center justify-center p-6 bg-gray-50">
-        <div className="flex w-full max-w-2xl bg-white rounded-xl shadow-xl border border-gray-200/80">
-          <main className="p-6 sm:p-12 w-full">
-            <h1 className="mb-4 text-xl font-semibold text-gray-700">Report</h1>
-            <form className="w-full" onSubmit={onSubmit}>
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div
+        style={{
+          display: "flex",
+          minHeight: "92.7vh",
+          flexDirection: "column",
+          width: "100%",
+        }}
+      >
+        <Container my="auto">
+          <Paper
+            radius="md"
+            p="xl"
+            withBorder
+            w={"34rem"}
+            sx={(theme) => {
+              return {
+                [theme.fn.smallerThan("xs")]: {
+                  width: 340,
+                },
+              };
+            }}
+          >
+            <Title
+              align="center"
+              mb={20}
+              sx={(theme) => ({
+                fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+                fontWeight: 600,
+              })}
+            >
+              Report
+            </Title>
+            <form
+              onSubmit={form.onSubmit(async (val) => {
+                setLoading(true);
+                const formData = new FormData();
+                formData.append("patient_id", val.patient_id);
+                formData.append("name", val.name);
+                formData.append("description", val.description);
+                formData.append("patient_age", val.patient_age);
+                formData.append("type", val.type);
+                formData.append("disease", val.disease);
+                formData.append("criticality", val.criticality);
+                formData.append("date", val.date);
+                formData.append("price", val.price);
+                formData.append("report", reportFile);
+                try {
+                  const { data } = await axios.post(
+                    `/api/registration/report/${type_of_user}`,
+                    formData
+                  );
+                  form.reset();
+                  setReportFile(null);
+                  setAlert({
+                    isAlert: true,
+                    type: "success",
+                    message: data,
+                  });
+                  navigate("/dashboard");
+                } catch (error) {
+                  setAlert({
+                    isAlert: true,
+                    type: "error",
+                    message: error.response.data,
+                  });
+                }
+                setLoading(false);
+              })}
+            >
+              <Stack
+                sx={(theme) => ({
+                  display: "grid",
+                  gridTemplateColumns: "1fr 1fr",
+                  [theme.fn.smallerThan("xs")]: {
+                    gridTemplateColumns: "1fr",
+                  },
+                })}
+              >
                 {type_of_user === "doctor" && (
-                  <div>
-                    <label className="input_label">Patient ID*</label>
-                    <input
-                      className="input_field"
-                      type="text"
-                      name="patient_id"
-                      value={register.patient_id}
-                      onChange={onChange}
-                      required
-                      list="patient_id_list"
-                    />
-                    <datalist id="patient_id_list">
-                      {patients.map((patient) => {
-                        return (
-                          <option value={patient._id}>{patient.name}</option>
-                        );
-                      })}
-                    </datalist>
-                  </div>
+                  <Select
+                    label="Patient ID"
+                    placeholder="Patient ID"
+                    data={patients.map((patient) => {
+                      return { value: patient._id, label: patient.name };
+                    })}
+                    value={form.values.patient_id}
+                    onChange={(event) =>
+                      form.setFieldValue(
+                        "patient_id",
+                        event
+                      )
+                    }
+                    required
+                  />
                 )}
-                <div>
-                  <label className="input_label">Report Name*</label>
-                  <input
-                    className="input_field"
-                    type="text"
-                    name="name"
-                    value={register.name}
-                    onChange={onChange}
-                    required
-                  />
-                </div>
-                <div>
-                  <label className="input_label">Description*</label>
-                  <input
-                    className="input_field"
-                    type="text"
-                    name="description"
-                    value={register.description}
-                    onChange={onChange}
-                    required
-                  />
-                </div>
-                <div>
-                  <label className="input_label">Patient Age*</label>
-                  <input
-                    className="input_field"
-                    type="text"
-                    name="patient_age"
-                    value={register.patient_age}
-                    onChange={onChange}
-                    required
-                  />
-                </div>
-                <div>
-                  <label className="input_label">Type of Report*</label>
-                  <select
-                    className="input_field"
-                    name="type"
-                    value={register.type}
-                    onChange={onChange}
-                    required
-                  >
-                    <option selected value="">
-                      Report Type
-                    </option>
-                    <option value="pdf">PDF</option>
-                    <option value="image">Image</option>
-                  </select>
-                </div>
-                <div>
-                  <label className="input_label">Disease*</label>
-                  <input
-                    className="input_field"
-                    type="text"
-                    name="disease"
-                    value={register.disease}
-                    onChange={onChange}
-                    required
-                  />
-                </div>
-                <div>
-                  <label className="input_label">Criticality*</label>
-                  <select
-                    className="input_field"
-                    name="criticality"
-                    value={register.criticality}
-                    onChange={onChange}
-                    required
-                  >
-                    <option selected value="">
-                      Criticality Level
-                    </option>
-                    <option value="high">High</option>
-                    <option value="medium">Medium</option>
-                    <option value="low">Low</option>
-                  </select>
-                </div>
-                <div>
-                  <label className="input_label">Date*</label>
-                  <input
-                    className="input_field"
-                    type="date"
-                    name="date"
-                    value={register.date}
-                    onChange={onChange}
-                    required
-                  />
-                </div>
+                <TextInput
+                  placeholder="Report Name"
+                  label="Report Name"
+                  value={form.values.name}
+                  onChange={(event) =>
+                    form.setFieldValue("name", event.currentTarget.value)
+                  }
+                  required
+                />
+                <TextInput
+                  placeholder="Description"
+                  label="Description"
+                  value={form.values.description}
+                  onChange={(event) =>
+                    form.setFieldValue("description", event.currentTarget.value)
+                  }
+                  required
+                />
+                <TextInput
+                  placeholder="Patient Age"
+                  label="Patient Age"
+                  value={form.values.patient_age}
+                  onChange={(event) =>
+                    form.setFieldValue("patient_age", event.currentTarget.value)
+                  }
+                  required
+                />
+                <Select
+                  label="Type of Report"
+                  placeholder="Select Type of Report"
+                  data={[
+                    { value: "pdf", label: "PDF" },
+                    { value: "image", label: "Image" },
+                  ]}
+                  value={form.values.type}
+                  onChange={(event) => form.setFieldValue("type", event)}
+                  required
+                />
+                <TextInput
+                  placeholder="Disease"
+                  label="Disease"
+                  value={form.values.disease}
+                  onChange={(event) =>
+                    form.setFieldValue("disease", event.currentTarget.value)
+                  }
+                  required
+                />
+                <Select
+                  label="Criticality"
+                  placeholder="Select Criticality Level"
+                  data={[
+                    { value: "high", label: "High" },
+                    { value: "medium", label: "Medium" },
+                    { value: "low", label: "Low" },
+                  ]}
+                  value={form.values.criticality}
+                  onChange={(event) => form.setFieldValue("criticality", event)}
+                  required
+                />
                 {type_of_user === "patient" && (
-                  <div>
-                    <label className="input_label">Price*</label>
-                    <input
-                      className="input_field"
-                      type="text"
-                      name="price"
-                      value={register.price}
-                      onChange={onChange}
-                      required
-                    />
-                  </div>
-                )}
-                <div>
-                  <label className="input_label">Upload Report*</label>
-                  <input
-                    className="input_field"
-                    type="file"
-                    name="report"
-                    onChange={onFileChange}
+                  <TextInput
+                    placeholder="Price"
+                    label="Price"
+                    value={form.values.price}
+                    onChange={(event) =>
+                      form.setFieldValue("price", event.currentTarget.value)
+                    }
                     required
                   />
-                </div>
-              </div>
-              <button type="submit" className="input_button">
-                Upload
-              </button>
+                )}
+                <DateInput
+                  valueFormat="YYYY MMM DD"
+                  label="Date"
+                  placeholder="Date"
+                  value={form.values.date}
+                  onChange={(event) => form.setFieldValue("date", event)}
+                  required
+                />
+                <FileInput
+                  label="Upload Report"
+                  placeholder="Upload Report"
+                  value={reportFile}
+                  onChange={setReportFile}
+                  required
+                />
+              </Stack>
+              <Button type="submit" radius="md" fullWidth color="cyan" mt={20}>
+                Upload Report
+              </Button>
             </form>
-          </main>
-        </div>
+          </Paper>
+        </Container>
       </div>
-    </div>
+    </>
   );
 };
 
